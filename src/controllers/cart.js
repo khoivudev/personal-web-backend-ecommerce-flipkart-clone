9bf41713dd39aa517f0b585ec98db330daf130c2
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-10-29 15:04:37 +0700

    fix cart api: add item to cart

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -8,42 +8,36 @@ exports.addItemToCart = (req, res) => {
         const product = req.body.cartItems.product;
         const item = cart.cartItems.find((c) => c.product == product);
 
+        let condition, update;
         if (item) {
-          Cart.findOneAndUpdate(
-            { user: req.user._id, "cartItems.product": product },
-            {
-              $set: {
-                cartItems: {
-                  ...req.body.cartItems,
-                  quantity: item.quantity + req.body.cartItems.quantity,
-                },
+          condition = { user: req.user._id, "cartItems.product": product };
+          update = {
+            $set: {
+              "cartItems.$": {
+                ...req.body.cartItems,
+                quantity: item.quantity + req.body.cartItems.quantity,
               },
             },
-            { useFindAndModify: false, new: true }
-          )
-            .then((cart) => {
-              return res.status(200).json(cart);
-            })
-            .catch((error) => {
-              return res.status(400).json(error);
-            });
+          };
         } else {
-          Cart.findOneAndUpdate(
-            { user: req.user._id },
-            {
-              $push: {
-                cartItems: req.body.cartItems,
-              },
+          condition = { user: req.user._id };
+          update = {
+            $push: {
+              cartItems: req.body.cartItems,
             },
-            { useFindAndModify: false, new: true }
-          )
-            .then((cart) => {
-              return res.status(200).json(cart);
-            })
-            .catch((error) => {
-              return res.status(400).json(error);
-            });
+          };
         }
+
+        Cart.findOneAndUpdate(condition, update, {
+          useFindAndModify: false,
+          new: true,
+        })
+          .then((cart) => {
+            return res.status(200).json(cart);
+          })
+          .catch((error) => {
+            return res.status(400).json(error);
+          });
       } else {
         // if cart not exists then create a new cart
         const cart = new Cart({
